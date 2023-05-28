c81db97492d5a38fec685e9dbd07ac99e58912cc
local_ubuntu <kanikama775@gmail.com>
2023-05-28 22:27:14 +0900

    :+1: Lambda,dynamodbに以下の変更を加えた       - Lambda関数(random_choice_text)を変更し、         Twitter内容にdescription属性を追加       - dynamodbテーブルのtext属性をcontents属性に変更         description属性を追加

--- terraform-twitter-bot/dynamodb.tf
+++ terraform-twitter-bot/dynamodb.tf
@@ -1,3 +1,8 @@
+variable "items" {
+  type = string
+  default = "./conf/item.json"
+}
+
 resource "aws_dynamodb_table" "dynamodb" {
   attribute {
     name = "hash"
@@ -21,15 +26,15 @@ resource "aws_dynamodb_table" "dynamodb" {
 
 # テーブルアイテムの設定
 locals {
-  json_data = file("./conf/table.json")         
+  json_data = file(var.items)
   config    = jsondecode(local.json_data)
 }
 
 resource "aws_dynamodb_table_item" "item" {
-  for_each = local.config                   
+  for_each = local.config
 
   table_name = aws_dynamodb_table.dynamodb.name
   hash_key   = aws_dynamodb_table.dynamodb.hash_key
 
-  item = jsonencode(each.value)             
+  item = jsonencode(each.value)
 }
