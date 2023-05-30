95d29facd99f78d9551a76a13cb867573ea9bb27
local_ubuntu <kanikama775@gmail.com>
2023-05-30 22:57:28 +0900

    :+1: Lambda関数に例外処理を追加

--- terraform-translate-serverless/app/lambda.py
+++ terraform-translate-serverless/app/lambda.py
@@ -2,6 +2,7 @@ import os
 import json
 import boto3
 import datetime
+import logging
 
 translate = boto3.client('translate')
 dynamodb = boto3.resource('dynamodb')
@@ -9,19 +10,31 @@ table = dynamodb.Table(os.environ['table_name'])
 
 def handler(event, context):
     input_text = event['queryStringParameters']['input_text']
-    response = translate.translate_text(
+    
+    try:
+        response = translate.translate_text(
         Text=input_text,
         SourceLanguageCode='ja',
         TargetLanguageCode='en'
     )
+    except Exception as e:
+        logging.error(e.response['Error']['Message'])
+        raise Exception("[ErrorMessage]: " + str(e))
+    
     output_text = response.get('TranslatedText')
-    table.put_item(
+
+    try:
+        table.put_item(
         Item = {
             'timestamp': datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
             'input_text': input_text,
             'output_text': output_text
         }
         )
+    except Exception as e:
+        logging.error(e.response['Error']['Message'])
+        raise Exception("[ErrorMessage]: " + str(e))
+
     return {
         'statusCode': 200,
         'body': json.dumps({
