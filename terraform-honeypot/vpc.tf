766c28910b0628b19edc0043057ff2a67375ab39
local_ubuntu <kanikama775@gmail.com>
2023-06-01 02:10:33 +0900

    :+1: ハニーポット用EC2を作成するTerraformコードを追加

---
+++ terraform-honeypot/vpc.tf
@@ -0,0 +1,41 @@
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${var.name}-vpc"
+  }
+}
+
+# パブリックサブネット
+resource "aws_subnet" "public" {
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.name}-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+# ルートテーブル
+resource "aws_route_table" "public-rtb" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.name}-public-rtb"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public-rtb.id
+}
