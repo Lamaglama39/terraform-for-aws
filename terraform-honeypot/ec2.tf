e6030a88fd88d90581de6ccb906ddccd95042c70
local_ubuntu <kanikama775@gmail.com>
2023-06-01 23:15:31 +0900

    :+1: terraform-honeypot       - eipを追加       - EC2セットアップ用の変数を追加

--- terraform-honeypot/ec2.tf
+++ terraform-honeypot/ec2.tf
@@ -12,13 +12,12 @@ data "aws_ami" "debian_11" {
 # honeypot server
 resource "aws_instance" "server" {
   ami           = data.aws_ami.debian_11.id
-  instance_type = "t3.xlarge"
+  instance_type = var.ec2_instance_type
   iam_instance_profile = aws_iam_instance_profile.systems_manager.name
   subnet_id            = aws_subnet.public.id
   vpc_security_group_ids = ["${aws_security_group.server.id}"]
-  # key_name               = aws_key_pair.key_pair.id
   associate_public_ip_address = true
-  user_data = file("./conf/user_data.sh")
+  user_data = templatefile("./conf/cloud-config.yml", { timezone = var.timezone, password = var.linux_password, tpot_flavor = var.tpot_flavor, web_user = var.web_user, web_password = var.web_password })
 
     # EBS最適化を有効
     ebs_optimized = "true"
@@ -26,7 +25,7 @@ resource "aws_instance" "server" {
     # EBSのルートボリューム設定
     root_block_device {
         # ボリュームサイズ(GiB)
-        volume_size = 300
+        volume_size = 150
         # ボリュームタイプ
         volume_type = "gp3"
         # # GP3のIOPS
@@ -44,4 +43,10 @@ resource "aws_instance" "server" {
   tags = {
     Name = "${var.name}-server"
   }
+}
+
+# パブリックIP
+resource "aws_eip" "eip" {
+  instance = aws_instance.server.id
+  vpc = true
 }
