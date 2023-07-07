f823a2d1879e500bef9a70f5a6cc4c9acc6b4db1
Lamaglama39 <lamaglama621@gmail.com>
2023-07-07 23:15:21 +0900

    :+1: terraform-stable-diffusion       - スポットリクエストのデフォルトの終了時間を追加       - spot_price用の変数追加

--- terraform-stable-diffusion/variables.tf
+++ terraform-stable-diffusion/variables.tf
@@ -6,5 +6,16 @@ variable "name" {
 
 # EC2 設定
 variable "ec2_instance_type" {
-  default = "g5.xlarge"
+  # default = "g5.xlarge"
+  default = "t3.micro"
+}
+
+# スポットインスタンスリクエスト 終了時間
+variable "end_time" {
+  default = "1h"
+}
+
+# スポットインスタンスリクエスト 最高価格
+variable "price" {
+  default = "0.6"
 }
