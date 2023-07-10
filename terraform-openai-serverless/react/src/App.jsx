59769bc1ae5b53ab312468cb1ef49f90e9d3679c
local_ubuntu <kanikama775@gmail.com>
2023-07-10 22:38:58 +0900

    :+1: terraform-openai-serverless react追加

---
+++ terraform-openai-serverless/react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import ChatDisplay from "./components/ChatDisplay";
+import ChatForm from "./components/ChatForm";
+import "./App.css";
+
+const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+
+  return (
+    <div>
+      <ChatDisplay chatHistory={chatHistory} />
+      <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} />
+    </div>
+  );
+};
+
+export default App;
