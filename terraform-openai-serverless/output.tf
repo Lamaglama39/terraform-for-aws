a8d2431cbd20290f85eb589c988225f1d930ba94
local_ubuntu <kanikama775@gmail.com>
2023-07-13 22:12:01 +0900

    :+1: terraform-openai-serverless       - favicon.ico 404エラーを回避

--- terraform-openai-serverless/output.tf
+++ terraform-openai-serverless/output.tf
@@ -1,15 +1,7 @@
-output "aws_lambda_function_arn" {
-  value = aws_lambda_function.lambda.arn
-}
-
-output "aws_lambda_function_name" {
-  value = aws_lambda_function.lambda.id
-}
-
 output "lambda_url" {
   value = "${aws_lambda_function_url.lambda_url.function_url}?api_model=gpt-3.5-turbo&system_text=YourOpenAI&user_text=Hello"
 }
 
 output "aws_cloudfront_distribution_domain_name" {
-  value = aws_cloudfront_distribution.cloudfront_distribution.domain_name
+  value = "https://${aws_cloudfront_distribution.cloudfront_distribution.domain_name}"
 }
