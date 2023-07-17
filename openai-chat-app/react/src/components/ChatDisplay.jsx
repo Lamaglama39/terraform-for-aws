86276bbad8e6e3cb371f339bcd39fd109989d09e
local_ubuntu <kanikama775@gmail.com>
2023-07-17 21:30:25 +0900

    :recycle: openai-chat-app             - CSSを別ファイルに分離             - UIの画面構成/色を修正

--- openai-chat-app/react/src/components/ChatDisplay.jsx
+++ openai-chat-app/react/src/components/ChatDisplay.jsx
@@ -1,16 +1,6 @@
-import { useState, useEffect } from "react";
-
 const ChatDisplay = ({ chatHistory }) => {
   return (
-    <div
-      style={{
-        overflow: "auto",
-        height: "calc(100vh - 100px)",
-        marginBottom: "20px",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
+    <div className="chat">
       {chatHistory.map((chat, index) => (
         <div key={index} className={`message ${chat.type}`}>
           <div dangerouslySetInnerHTML={{ __html: chat.message }}></div>
