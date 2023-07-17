63e5e2082e9bf4b3edc46ddb8e6914b736b2bcc7
Lamaglama39 <lamaglama621@gmail.com>
2023-07-17 22:41:21 +0900

    :+1: openai-chat-app       - UIのチャットメッセージにアイコン追加

--- openai-chat-app/react/src/components/ChatDisplay.jsx
+++ openai-chat-app/react/src/components/ChatDisplay.jsx
@@ -1,8 +1,16 @@
+import UserIcon from "../icon/animal_arupaka.png"; // ユーザーアイコンのパス
+import SystemIcon from "../icon/ai_dance_character.png"; // システムアイコンのパス
+
 const ChatDisplay = ({ chatHistory }) => {
   return (
     <div className="chat">
       {chatHistory.map((chat, index) => (
         <div key={index} className={`message ${chat.type}`}>
+          <img
+            src={chat.type === "user" ? UserIcon : SystemIcon}
+            className="icon"
+            alt="icon"
+          />
           <div dangerouslySetInnerHTML={{ __html: chat.message }}></div>
         </div>
       ))}
