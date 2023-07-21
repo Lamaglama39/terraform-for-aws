2a44a655780e0c4112f9b795c1324a54436204a4
Lamaglama39 <lamaglama621@gmail.com>
2023-07-21 21:26:02 +0900

    :sparkles: openai-chat-app             - UI側に初回アクセス、またはリロード時に、               過去のチャット履歴を呼び出す処理を追加

--- openai-chat-app/react/src/App.jsx
+++ openai-chat-app/react/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ChatDisplay from "./components/ChatDisplay";
 import ChatForm from "./components/ChatForm";
+import useLoadChatApi from "./components/useLoadChatApi";
 import "./App.css";
 
 const App = () => {
+  const loadUrl = import.meta.env.VITE_LAMBDA_URL;
+  const loadParams = { load: true };
   const [chatHistory, setChatHistory] = useState([]);
+  const { isLoading, fetchChatResponse } = useLoadChatApi();
+
+  const onSuccess = (messages) => {
+    setChatHistory(messages);
+  };
+
+  // 初回ロード時にfetchChatResponseを呼び出す
+  useEffect(() => {
+    fetchChatResponse(loadUrl, loadParams, onSuccess);
+  }, []);
+
   return (
     <div className="page">
       <ChatDisplay chatHistory={chatHistory} />
