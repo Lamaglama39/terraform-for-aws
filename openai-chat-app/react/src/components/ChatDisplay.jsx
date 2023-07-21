43662a3eb9e129ccef581529e0354d3aff13ae8b
local_ubuntu <kanikama775@gmail.com>
2023-07-21 22:14:22 +0900

    :+1: openai-chat-app       - チャットメッセージがふわっと出現する処理を追加

--- openai-chat-app/react/src/components/ChatDisplay.jsx
+++ openai-chat-app/react/src/components/ChatDisplay.jsx
@@ -1,16 +1,27 @@
-import React, { useEffect, useRef } from "react"; // 追加
+import React, { useEffect, useRef } from "react";
 import UserIcon from "../icon/animal_arupaka.png";
 import SystemIcon from "../icon/ai_dance_character.png";
 
 const ChatDisplay = ({ chatHistory }) => {
-  const chatRef = useRef(null); // 追加
+  const chatRef = useRef(null);
 
   // 最下部へのスクロール
   useEffect(() => {
     if (chatRef.current) {
       chatRef.current.scrollTop = chatRef.current.scrollHeight;
+
+      // ふわっと出現する効果を適用
+      const messages = chatRef.current.getElementsByClassName("message");
+      for (const message of messages) {
+        if (!message.classList.contains("show")) {
+          // 一定時間後に.showクラスを追加する
+          setTimeout(() => {
+            message.classList.add("show");
+          }, 100);
+        }
+      }
     }
-  }, [chatHistory]); // chatHistoryの変更を監視
+  }, [chatHistory]);
 
   return (
     <div className="chat" ref={chatRef}>
