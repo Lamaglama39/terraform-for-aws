8ae5369505b53975cab47856a8168d17667fa645
local_ubuntu <kanikama775@gmail.com>
2023-07-27 21:27:04 +0900

    :+1: openai-chat-app       - UI全体の調整       - チャット履歴削除時の確認処理を追加

--- openai-chat-app/react/src/components/ChatForm.jsx
+++ openai-chat-app/react/src/components/ChatForm.jsx
@@ -54,6 +54,11 @@ const ChatForm = ({ chatHistory, setChatHistory }) => {
   // 履歴削除処理
   const handleDelete = (event) => {
     event.preventDefault();
+    const isConfirmed = window.confirm("本当に履歓を削除しますか？");
+    if (!isConfirmed) {
+      // ユーザーがキャンセルを選択した場合、処理を中止
+      return;
+    }
 
     // CloudFront ディストリビューションドメイン名
     const url = import.meta.env.VITE_LAMBDA_URL;
@@ -76,32 +81,31 @@ const ChatForm = ({ chatHistory, setChatHistory }) => {
 
   return (
     <>
-      <form onSubmit={handleDelete} className="delete-btn">
-        <input
-          type="submit"
-          value="チャット履歴削除"
-          style={{
-            width: "width: 100%",
-          }}
-        />
-      </form>
+      <div className="top-form">
+        <form onSubmit={handleDelete}>
+          <input
+            type="submit"
+            value="チャット履歴削除"
+            className="delete-btn"
+          />
+        </form>
+        <select
+          name="api_model"
+          className="api-model-select"
+          value={formState.api_model}
+          onChange={handleInputChange}
+        >
+          <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
+          <option value="gpt-3.5-turbo-16k">gpt-3.5-turbo-16k</option>
+          <option value="gpt-3.5-turbo-0613">gpt-3.5-turbo-0613</option>
+          <option value="gpt-3.5-turbo-16k-0613">gpt-3.5-turbo-16k-0613</option>
+        </select>
+      </div>
       <form onSubmit={handleSubmit} className="bottom-form">
         <div className="form-wrap">
           <div className="form-area system-form">
             <label style={{ marginRight: "10px" }}>
-              AIの役割/性格:
-              <select
-                name="api_model"
-                value={formState.api_model}
-                onChange={handleInputChange}
-              >
-                <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
-                <option value="gpt-3.5-turbo-16k">gpt-3.5-turbo-16k</option>
-                <option value="gpt-3.5-turbo-0613">gpt-3.5-turbo-0613</option>
-                <option value="gpt-3.5-turbo-16k-0613">
-                  gpt-3.5-turbo-16k-0613
-                </option>
-              </select>
+              AIの役割/性格
               <textarea
                 placeholder="AIの役割とか性格を入力してね。"
                 name="system_text"
@@ -113,7 +117,7 @@ const ChatForm = ({ chatHistory, setChatHistory }) => {
           </div>
           <div className="form-area user-form">
             <label>
-              チャット内容:
+              チャット内容
               <textarea
                 className="text-area"
                 placeholder="チャット内容を入力してね。"
@@ -123,6 +127,7 @@ const ChatForm = ({ chatHistory, setChatHistory }) => {
               ></textarea>
             </label>
             <input
+              className="submit-btn"
               type="submit"
               value="送信"
               style={{
